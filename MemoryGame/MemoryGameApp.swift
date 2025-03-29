ba1aa40de50a45a547369f349d182d182ef6c1ad
Samuel Lopez <samuelarte35@gmail.com>
2025-03-28 19:09:02 -0500

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Erisanny Vargas on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryGameTests/MemoryGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameTests.swift
+//  MemoryGameTests
+//
+//  Created by Erisanny Vargas on 3/28/25.
+//
+
+import Testing
+@testable import MemoryGame
+
+struct MemoryGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
